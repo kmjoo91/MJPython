a3d407fa3d41c68790877d3fea5b7311aa7c7560
KimMinJoo <kmjoo91@naver.com>
2015-09-14 13:55:53 +0900

    MJ0914
    9월14일 실습

---
+++ MJ0914/MJ0914/MJ0914.py
@@ -0,0 +1,25 @@
+﻿#배포패키지만들기
+import os
+print(os.getcwd())
+#os.mkdir("sample")
+os.chdir(".//sample")
+print(os.getcwd())
+#os.system("python setup.py sdist")
+os.system("python setup.py install")
+#함수 예제
+'''
+def sum_and_mul(a,b):
+   return a+b,a*b
+#answer = sum_and_mul(10,30)
+sum,mul = sum_and_mul(10,30)
+print(sum,mul)
+'''
+#모듈만들기
+'''
+import printData
+
+movie=['홍길동',['베테랑',['류승완','황정민','유아인']],
+       '고길동',['앤트맨','베테랑'],
+       '김길동',['협녀','암살']]
+printData.printData(movie)
+'''
